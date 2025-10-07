45d6fdcc3cbf29c358c5055d680af250733571b5
kirkm <mmmm_donut@msn.com>
2025-10-07 14:09:36 -0600

    make compat with python3.9

--- modulev1.py
+++ modulev1.py
@@ -9,9 +9,16 @@ cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 cap.set(cv2.CAP_PROP_BRIGHTNESS, 0.5)
 cap.set(cv2.CAP_PROP_CONTRAST, 0.8)
 
+if not cap.isOpened():
+    print('camera done not work')
+    exit()
+
 while True:
     ret, frame = cap.read()
-    cv2.imshow(frame)
+    print(ret)
+    if not ret:
+        print('end of vid or cant read')
+    cv2.imshow('Camera', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
